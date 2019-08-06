e80f84891ade25cf2c29b812c7b528e8f3692fe8
Esteban <juan.castano@globant.com>
2019-08-06 09:27:48 -0500

    upgraded to django 2.2 and fixed migration issues

--- post/urls.py
+++ post/urls.py
@@ -2,21 +2,23 @@
 Urls module
 '''
 
-from django.conf.urls import url
+from django.urls import path
 
 from post.views import post as post_views
 from post.views import comment as comment_views
 
+app_name='post'
+
 urlpatterns = [  
-    url(r'^$', post_views.HomeView.as_view(),
+    path(r'', post_views.HomeView.as_view(),
         name='index'),
-    url(r'^new', post_views.PostView.as_view(),
+    path(r'new', post_views.PostView.as_view(),
         name='new'),
-    url(r'^edit/(?P<id>\d+)/$', post_views.EditPostVIew.as_view(),
+    path(r'edit/(?P<id>\d+)/$', post_views.EditPostVIew.as_view(),
         name='edit'),
-    url(r'^delete/(?P<id>\d+)/$', post_views.DeletePostView.as_view(),
+    path(r'delete/(?P<id>\d+)/$', post_views.DeletePostView.as_view(),
         name='delete'),
-    url(r'^list/(?P<id>\d+)/$', comment_views.CommentListView.as_view(),
+    path(r'list/(?P<id>\d+)/$', comment_views.CommentListView.as_view(),
         name='list'),
     
 ]
